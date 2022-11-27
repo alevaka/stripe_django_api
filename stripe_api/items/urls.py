eb5f9a2bd06180d9d63adc05b5e6ee55e8d6080a
Alexey Kachkaev <akachkaev@gmail.com>
2022-11-28 03:03:57 +0500

    Add order operations

--- stripe_api/items/urls.py
+++ stripe_api/items/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import buy_item, create_order, show_item
+from .views import (add_item_to_order, buy_item, create_order, pay_order,
+                    show_item, show_order)
 
 urlpatterns = [
     path('buy/<int:pk>/', buy_item),
+    path('item/<int:pk>/add_to_order/', add_item_to_order),
     path('item/<int:pk>/', show_item),
     path('new_order/', create_order),
+    path('order/<int:pk>/pay/', pay_order),
+    path('order/<int:pk>/', show_order),
 ]
--- stripe_api/items/views.py
+++ stripe_api/items/views.py
@@ -1,9 +1,9 @@
 import os
+import json
 
 import stripe
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
-
 
 from .models import Item, Order
 
@@ -12,7 +12,7 @@ stripe_api_key = os.getenv('STRIPE_API_PUBLIC_KEY')
 
 def buy_item(request: HttpRequest, pk: int) -> JsonResponse:
     """Функция для покупки одного товара.
-    Создаёт Stripe Session и возвращает id сессии"""
+       Создаёт Stripe Session и возвращает id сессии."""
 
     line_items = []
     item = get_object_or_404(Item, pk=pk)
@@ -62,7 +62,84 @@ def show_item(request: HttpRequest, pk: int) -> HttpResponse:
 
     return render(request, template, context)
 
+
 def create_order(request: HttpRequest) -> JsonResponse:
+    """Создает новый заказ и возвращает его id."""
+
     order = Order.objects.create()
-    meta_keys = request.META
-    return JsonResponse({'order_id': order.id, 'REMOTE_ADDR': meta_keys['REMOTE_ADDR']})
+    return JsonResponse({'order_id': order.id})
+
+
+def add_item_to_order(request: HttpRequest, pk: int) -> JsonResponse:
+    """Добавляет товар в заказ.
+       Номер заказа необходимо передать в теле запроса."""
+
+    order_id = json.loads(request.body)['order_id']
+    order = get_object_or_404(Order, id=order_id)
+    item = get_object_or_404(Item, pk=pk)
+    order.item.add(item)
+    items = []
+    for item in order.item.all():
+        items.append(str(item))
+    return JsonResponse({'order_items': items})
+
+
+def exchange_rate(currency_from: str, currency_to: str) -> int:
+    """Конвертация валют.
+       Для тестирования примем, что у нас только две валюты
+       и курс у них стабилен."""
+    rates = {
+        'USD_RUB': 60,
+        'RUB_USD': 0.0166667,
+    }
+    return rates.get(f'{currency_from}_{currency_to}') or 9999
+
+
+def pay_order(request: HttpRequest, pk: int) -> JsonResponse:
+    """Оплата заказа. Цена всех товаров конвертируется в валюту первого товара.
+    Создаёт Stripe Session и возвращает id сессии"""
+
+    order = get_object_or_404(Order, pk=pk)
+    line_items = []
+    first_currency = None
+    for item in order.item.all():
+        first_currency = first_currency or item.currency
+        converted_price = (item.price if first_currency == item.currency
+                           else exchange_rate(item.currency, first_currency)
+                           * item.price)
+        line_items.append(
+            {
+              'price_data': {
+                'currency': first_currency,
+                'product_data': {
+                  'name': item.name,
+                },
+                'unit_amount': int(converted_price * 100)
+              },
+              'quantity': 1,
+            },
+        )
+
+    session = stripe.checkout.Session.create(
+          api_key=stripe_api_key,
+          mode='payment',
+          line_items=line_items,
+          success_url=f'http://localhost:8000/order/{pk}/',
+          cancel_url=f'http://localhost:8000/order/{pk}/'
+      )
+
+    json_data = {
+      'pk': pk,
+      'session_id': session.id
+    }
+    return JsonResponse(json_data)
+
+
+def show_order(request: HttpRequest, pk: int) -> JsonResponse:
+    """Возвращает JSON списка товаров в заказе"""
+
+    order = get_object_or_404(Order, pk=pk)
+    items = []
+    for item in order.item.all():
+        items.append(str(item))
+    return JsonResponse({'order_items': items})
